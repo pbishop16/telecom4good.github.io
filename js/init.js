ffa4b75eb1e714271100ca1f6d80fb8170074f92
Ari Perez <ariperez83@gmail.com>
2016-07-27 11:46:43 -0400

    Fix Carousel loading

--- js/init.js
+++ js/init.js
@@ -2,7 +2,11 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+    $('.parallax').parallax();   
+    $('.carousel').carousel({
+    	full_width: true,
+    	indicators: true
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
