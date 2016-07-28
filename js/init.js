3a64660ab86b6d44321c453745841ea12607b681
Ari Perez <ariperez83@gmail.com>
2016-07-28 11:48:01 -0400

    Improved Carousel

--- js/init.js
+++ js/init.js
@@ -3,14 +3,8 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();   
-    $('.carousel').carousel({
-    	full_width: true,
-    	indicators: true
-    });
-
-    setInterval(function(){
-		  $('.carousel').carousel('next');
-		}, 4500);
+    $('.slider').slider({height: 550});
+    
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
