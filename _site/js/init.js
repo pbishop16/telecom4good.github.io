39d9d744b14034c61720358866ce48cdc5025290
Ari Perez <ariperez83@gmail.com>
2016-08-07 18:49:12 -0400

    #15 No Visual Changes; Navbar and Footer code are shared across pages

---
+++ _site/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();   
+    $('.slider').slider({full_width: true, height: 500});
+    $(".dropdown-button").dropdown({hover: true});
+    
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
